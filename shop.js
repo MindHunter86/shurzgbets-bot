860bbd078ec0234ce018f4f1e03bcc8d58b471cc
Sergo <sergo@mh00.net>
2016-03-18 01:08:19 +0300

    '1'

--- shop.js
+++ shop.js
@@ -31,6 +31,7 @@ try {
         logOnOptions.auth_code = authCode;
     }
 }
+console.log('Код магазин:', logOnOptions.two_factor_code);
 function getSHA1(bytes) {
     var shasum = crypto.createHash('sha1');
     shasum.end(bytes);
