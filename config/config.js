3b7e21278d91dd356e975e1c44f8a079e0e866e0
Vadimka Komissarov <vadimka_kom@mail.ru>
2016-05-18 15:33:43 +0000

    config.js edited online with Bitbucket

--- config/config.js
+++ config/config.js
@@ -7,21 +7,21 @@ var config = {
     prefix: '',
 	//настройка бота рулетки
     bot: {
-        username: 'tmallidaxinnga',
-        password: 'mRZesXvuqzTlFb'
+        username: '',
+        password: ''
     },
 	//настройки бота магазина
     shopBot: {
-        username: 'zettanalurzaish',
-        password: 'T3bqAUPrhUB5fNL',//
+        username: '',
+        password: '',//
         timeForCancelOffer: 1800
     },
     apiKey: '89638B050C0254D97337012787F57F68',	//steam api key
     domain: 'shurzgbets.com',	//домен сайта
-    secretKey: '1K2wmLstWcMTirHs2rEeOKvyxDTkZVCclceg41Qqb2f6QJ2FaDEWtoUTpMjgBtjY',
+    secretKey: '',
     
     admins: [	//steam id админов
-        '76561198180956505'
+        ''
     ]
 }
 
